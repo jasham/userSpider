2a945283d6686596065526785c0406d7dbd0c383
jasham009 <jasham009@gmail.com>
2021-04-09 00:46:18 +0530

    make it better

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+  target: 'server',
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
