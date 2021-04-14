ca1a36aceecb6dae7318523554eff9d1e2a8dac0
jasham009 <jasham009@gmail.com>
2021-04-14 23:53:43 +0530

    updating files

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  // target: 'server',
+  target: 'serverless',
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
